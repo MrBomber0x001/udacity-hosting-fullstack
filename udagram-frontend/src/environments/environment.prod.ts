664e80ba02d1fc514eba4d6542b80fd0d0363924
Yousef Meska <mrbomberboy123@gmail.com>
2022-04-25 20:04:11 +0200

    change eb api link

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://udagramapi-env.eba-zgwzx6cp.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
